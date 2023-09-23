67bea9de591c8de8a3ebb0f4bc7cdd46da98bbe9
Donato Silva <figueroajesus2015@hotmail.com>
2023-09-22 22:25:55 -0500

    Comparto mi codigo (Jesus Donato)

--- php_crud/modulos/contacto/editar.php
+++ php_crud/modulos/contacto/editar.php
@@ -1,74 +1,37 @@
-<?php 
-
-include("../../conexion.php");
-
-if(isset($_GET['id'])){
-
-    $txtid=(isset($_GET['id'])?$_GET['id']:"");
-    $stm=$conexion->prepare("SELECT * FROM enployee WHERE id=:txtid");
-    $stm->bindparam(":txtid",$txtid);    
-    $stm->execute();
-    $registro=$stm->fetch(PDO::FETCH_LAZY);
-    $cedula=$registro['cedula'];
-    $nombre=$registro['nombre'];
-    $apellido=$registro['apellido'];
-    $direccion=$registro['direccion'];
-    $telefono=$registro['telefono'];
-
-    }
-
-    if ($_POST){
-
-        $txtid=(isset($_POST["txtid"])?$_POST["txtid"]:"");
-        $cedula=(isset($_POST["cedula"])?$_POST["cedula"]:"");
-        $nombre=(isset($_POST["nombre"])?$_POST["nombre"]:"");
-        $apellido=(isset($_POST["apellido"])?$_POST["apellido"]:"");
-        $direccion=(isset($_POST["direccion"])?$_POST["direccion"]:"");
-        $telefono=(isset($_POST["telefono"])?$_POST["telefono"]:"");
-    
-        $stm=$conexion->prepare("UPDATE enployee SET cedula=:cedula,nombre=:nombre,apellido=:apellido,direccion=:direccion,telefono=:telefono WHERE id=:txtid");
-        $stm->bindparam(":txtid",$txtid);
-        $stm->bindparam(":cedula",$cedula);
-        $stm->bindparam(":nombre",$nombre);
-        $stm->bindparam(":apellido",$apellido);
-        $stm->bindparam(":direccion",$direccion);
-        $stm->bindparam(":telefono",$telefono);
-        $stm->execute();
-    
-        header("Location:index.php");
-    
-       
-    }
-
-
-?>
-
-<?php include("../../template/header.php"); ?>
-
-<form action="" method="post">
-      
-       
-        <input type="hidden" class="form-control" name="txtid" value="<?php echo $txtid?>" placeholder="Ingrese cedula">
-        <label for="">Cedula</label>
-        <input type="text" class="form-control" name="cedula" value="<?php echo $cedula?>" placeholder="Ingrese cedula">
-
-        <label for="">Nombre</label>
-        <input type="text" class="form-control" name="nombre" value="<?php echo $nombre?>" placeholder="Ingrese nombre">
-
-        <label for="">Apellido</label>
-        <input type="text" class="form-control" name="apellido" value="<?php echo $apellido?>" placeholder="Ingrese apellido">
-
-        <label for="">Direccion</label>
-        <input type="text" class="form-control" name="direccion" value="<?php echo $direccion?>" placeholder="Ingrese direccion">
-
-        <label for="">Telefono</label>
-        <input type="text" class="form-control" name="telefono" value="<?php echo $telefono?>" placeholder="Ingrese telefono">
-
-      </div>
-      <div class="modal-footer">
-         <a href="index.php"class="btn btn-danger">Cancelar</a> 
-         <button type="submit" class="btn btn-primary">Actualizar</button>
-      </div>
-      </form>
-
-      <?php include("../../template/footer.php"); ?>
+<!-- HTML para el offcanvas -->
+<div class="offcanvas offcanvas-end" tabindex="-1" id="editarEmpleadoOffcanvas" aria-labelledby="editarEmpleadoOffcanvasLabel">
+    <div class="offcanvas-header pt-4 pb-4">
+        <h5 class="offcanvas-title" id="editarEmpleadoOffcanvasLabel">Edit employee</h5>
+        <button type="button" class="btn-close-offcanvas border-0 rounded-2 p-1" data-bs-dismiss="offcanvas" aria-label="Cerrar"><i class="bi bi-x-lg pe-1 ps-1"></i></button>
+    </div>
+    <div class="offcanvas-body">
+        <form action="" method="post">
+            <input type="hidden" class="form-control rounded-4" name="txtid" value="" id="txtidEdit">
+            <div class="mb-3 row">
+                <div class="col-md-6 pe-1">
+                <label for="nombreEdit">Nombre</label>
+            <input type="text" class="form-control rounded-4" name="nombre" value="" placeholder="Ingrese nombre" id="nombreEdit">
+                </div>
+                <div class="col-md-6 ps-1">
+                    <label for="apellidoEdit">Apellido</label>
+                    <input type="text" class="form-control rounded-4" name="apellido" value="" placeholder="Ingrese apellido" id="apellidoEdit">
+                </div>
+            </div>
+            <label for="direccionEdit">Direccion</label>
+            <input type="text" class="form-control rounded-4  mb-3" name="direccion" value="" placeholder="Ingrese direccion" id="direccionEdit">
+            <div class="mb-3 row">
+                <div class="col-md-6 pe-1">
+                    <label for="cedulaEdit">ID Employee</label>
+                    <input type="text" class="form-control rounded-4" name="cedula" value="" placeholder="Ingrese cedula" id="cedulaEdit">
+                </div>
+                <div class="col-md-6 ps-1">
+                    <label for="TelefonoEdit">Phone</label>
+                    <input type="text" class="form-control rounded-4" name="telefono" value="" placeholder="Ingrese telefono" id="telefonoEdit">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button name="update_employee"  type="submit" class="btn btn-primary rounded-4">Update <i class="bi bi-floppy-fill ms-3"></i></button>
+            </div>
+        </form>
+    </div>
+</div>
--- php_crud/template/header.php
+++ php_crud/template/header.php
@@ -1,33 +1,29 @@
-<?php $url_base="http://localhost/php_crud/" ?>
-
-
-
 <!doctype html>
-<html lang="en">
+<html lang="es">
 
 <head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Employee</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="scr\aries.png" type="image/x-icon">
+    <!--Font Roboto -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    
+    <!-- Styles for employees -->
+    <link rel="stylesheet" href="scr\css\style.css">
 
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
+    <!-- Javascript -->
+    <script src="scr\Js\funtion.js"></script>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <ul class="nav navbar-nav">
-          <li class="nav-item">
-              <a class="nav-link active" href="#" aria-current="page">GESTION DE EMPLEADOS <span class="visually-hidden">(current)</span></a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo $url_base;?>/modulos/contacto/">Empleados</a>
-          </li>
-      </ul>
-  </nav>
-
-  <main class="container">
-    <br><br><br><br>
+    <main class="container pt-3">
+        
